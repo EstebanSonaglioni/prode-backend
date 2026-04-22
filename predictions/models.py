8ce953d74ae0886a147e817297fd3975bd391713
arazard <arazard.dev@gmail.com>
2026-04-22 10:34:46 -0300

    register user endpoint added

---
+++ predictions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    flag_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
+    date_time = models.DateTimeField()
+    home_score = models.IntegerField(null=True, blank=True)
+    away_score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.home_team} vs {self.away_team}"
---
+++ users/views.py
@@ -0,0 +1,39 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from django.conf import settings
+from .serializers import RegisterSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+
+from django.contrib.auth.models import User
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+            # Seteamos la cookie HttpOnly
+            response.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=False, # Ponelo en True cuando subas a Render (HTTPS)
+                samesite='Lax'
+            )
+        return response
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,) # Cualquiera puede registrarse
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "user": serializer.data["username"],
+                "message": "Usuario creado exitosamente. Ahora puedes loguearte."
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
